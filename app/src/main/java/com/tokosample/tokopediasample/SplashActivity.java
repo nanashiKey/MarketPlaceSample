b7af76f7d25afd4aed877f19ff46c7a17fc691e1
Irfan Asidiq <assidiq.irfan@gmail.com>
2022-01-13 17:46:26 +0700

    splashactivity selesai

--- app/src/main/java/com/tokosample/tokopediasample/SplashActivity.java
+++ app/src/main/java/com/tokosample/tokopediasample/SplashActivity.java
@@ -1,7 +1,9 @@
 package com.tokosample.tokopediasample;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,5 +17,13 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         MethodHelper.setNotificationbar(this, R.color.white);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 2500);
     }
 }
